84400fb6c0d3c884e150dbec7575a61f7c0a9780
mchen434 <mchen434@gatech.edu>
2020-03-25 20:11:29 -0600

    finished button pressing functionality. TODO: route button pressing to changing up/downvote numbers in db

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -12,15 +12,53 @@ const Comment = props => {
     props.onComment(props.comment._id, commentData)
   }
 
+  const[upvote, setUpvote] = React.useState(false)
+  const[downvote, setDownvote] = React.useState(false)
+  const[totVotes, setTotVotes] = React.useState(props.comment.upVotes - props.comment.downVotes)
+
+  const clickUpvote = () => {
+    if (!upvote) { //upvote is not toggled and is being toggled
+      if (downvote) {//the downvote button was on
+        setDownvote(!downvote)
+        //raise by one then raise by one
+        setTotVotes(totVotes+2)
+      } else {//the downvote button was off
+        //raise by one
+        setTotVotes(totVotes+1)
+      }
+    } else { // the upvote has been toggled off
+      //decrement by one
+      setTotVotes(totVotes-1)
+    }
+    setUpvote(!upvote)//NOTE: state change is async, so do it at the end of non async operation
+  }
+
+  const clickDownvote = () => {
+    if (!downvote) {//downvote is not toggled and is being toggled
+      if (upvote) {
+        setUpvote(!upvote)
+        //decrement by one then decrement by one
+        setTotVotes(totVotes-2)
+      } else {
+        //decrement by one
+        setTotVotes(totVotes-1)
+      }
+    } else { //the downvote button has been toggled off
+      //increment by one
+      setTotVotes(totVotes+1)
+    }
+    setDownvote(!downvote)
+  }
+
   return (
     <>
       <section className="post">
         <div className="arrows">
-          <button>↑</button>
+          <button onClick = {clickUpvote} className={upvote ? "upSelected":""}>↑</button>
           <span className="center">
-            {props.comment.upVotes - props.comment.downVotes}
+            {totVotes}
           </span>
-          <button>↓</button>
+          <button onClick = {clickDownvote} className={downvote ? "downSelected":""}>↓</button>
         </div>
         <div className="post-body">
           <div className="author">Posted by {props.comment.author}</div>
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -4,6 +4,7 @@ import AddComment from './AddComment'
 import './Post.css'
 
 const Post = props => {
+  //Reply State
   const [replyOpen, setReplyOpen] = React.useState(false)
 
   const toggleReply = () => setReplyOpen(!replyOpen)
@@ -12,16 +13,57 @@ const Post = props => {
     setReplyOpen(false)
     props.onComment(props.post._id, commentData)
   }
+  //Up/down vote State
+  //true/false vars for up down vote
+  //global original vote variable?
+    
+  const[upvote, setUpvote] = React.useState(false)
+  const[downvote, setDownvote] = React.useState(false)
+  const[totVotes, setTotVotes] = React.useState(props.post.upVotes - props.post.downVotes)
+
+  const clickUpvote = () => {
+    if (!upvote) { //upvote is not toggled and is being toggled
+      if (downvote) {//the downvote button was on
+        setDownvote(!downvote)
+        //raise by one then raise by one
+        setTotVotes(totVotes+2)
+      } else {//the downvote button was off
+        //raise by one
+        setTotVotes(totVotes+1)
+      }
+    } else { // the upvote has been toggled off
+      //decrement by one
+      setTotVotes(totVotes-1)
+    }
+    setUpvote(!upvote)//NOTE: state change is async, so do it at the end of non async operation
+  }
+
+  const clickDownvote = () => {
+    if (!downvote) {//downvote is not toggled and is being toggled
+      if (upvote) {
+        setUpvote(!upvote)
+        //decrement by one then decrement by one
+        setTotVotes(totVotes-2)
+      } else {
+        //decrement by one
+        setTotVotes(totVotes-1)
+      }
+    } else { //the downvote button has been toggled off
+      //increment by one
+      setTotVotes(totVotes+1)
+    }
+    setDownvote(!downvote)
+  }
 
   return (
     <>
       <section className="post">
         <div className="arrows">
-          <button>↑</button>
+          <button onClick = {clickUpvote} className={upvote ? "upSelected":""}>↑</button>
           <span className="center">
-            {props.post.upVotes - props.post.downVotes}
+            {totVotes}
           </span>
-          <button>↓</button>
+          <button onClick = {clickDownvote} className={downvote ? "downSelected":""}>↓</button>
         </div>
         <div className="post-body">
           <div className="author">Posted by {props.post.author}</div>
